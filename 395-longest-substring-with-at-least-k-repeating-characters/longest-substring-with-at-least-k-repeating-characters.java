0564c432b5f700f7f42055a3f0a67178abbcac80
KHUSHBU MANDAL <131984812+KhushbuMandal@users.noreply.github.com>
2026-08-29 18:46:49 +0530

    Time: 316 ms (16.14%) | Memory: 43.8 MB (47.57%) - LeetSync

--- 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.java
+++ 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.java
@@ -3,34 +3,53 @@ class Solution {
 
         int n = s.length();
 
+        // prefix[i][j] =
+        // frequency of character j in first i characters
+        int[][] prefix = new int[n + 1][26];
+
+        // Build prefix frequency
+        for (int i = 0; i < n; i++) {
+
+            // Copy previous frequencies
+            for (int j = 0; j < 26; j++) {
+                prefix[i + 1][j] = prefix[i][j];
+            }
+
+            // Add current character
+            int index = s.charAt(i) - 'a';
+            prefix[i + 1][index]++;
+        }
+
         int maxLen = 0;
 
-        for (int i = 0 ; i < n ; i++) {
+        // Try every substring
+        for (int left = 0; left < n; left++) {
 
-            int freq[] = new int[26];
-
-            for (int j = i ; j < n ; j++) {
-
-                char c = s.charAt(j);
-                freq[c - 'a']++;
+            for (int right = left; right < n; right++) {
 
                 boolean valid = true;
-                for (int v = 0 ; v <= 25 ; v++) {
-                    if (freq[v] > 0 && freq[v] < k) {
+
+                // Check all 26 characters
+                for (int c = 0; c < 26; c++) {
+
+                    int freq = prefix[right + 1][c]
+                             - prefix[left][c];
+
+                    if (freq > 0 && freq < k) {
                         valid = false;
                         break;
                     }
                 }
 
                 if (valid) {
-                    maxLen = Math.max(maxLen , j - i + 1);
+                    maxLen = Math.max(
+                        maxLen,
+                        right - left + 1
+                    );
                 }
-
             }
-
         }
 
         return maxLen;
-        
     }
 }
