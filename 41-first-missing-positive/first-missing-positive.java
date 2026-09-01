f036117dc42d896298302e4e3c9a54eb84028e7f
KHUSHBU MANDAL <131984812+KhushbuMandal@users.noreply.github.com>
2026-09-01 12:16:20 +0530

    Time: 1 ms (99.96%) | Memory: 71.2 MB (72.85%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -3,15 +3,14 @@ class Solution {
 
         int n = nums.length;
 
-        for (int i  = 0 ; i < n ; i++) {
-
-            while (nums[i] >= 1 && nums[i] <= n && nums[nums[i] - 1] != nums[i] ) {
-                int correctIndex = nums[i] - 1;
+        for (int i = 0 ; i < n ; i++) {
+            
+            while (nums[i] >= 1 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                int idx = nums[i] - 1;
 
                 int temp = nums[i];
-                nums[i] = nums[correctIndex];
-                nums[correctIndex] = temp;
-
+                nums[i] = nums[idx];
+                nums[idx] = temp;
             }
 
         }
@@ -19,8 +18,8 @@ class Solution {
         for (int i = 0 ; i < n ; i++) {
             if (nums[i] != i + 1) {
                 return i + 1;
-            } 
+            }
         }
-        return n + 1;
+        return n + 1; 
     }
 }
