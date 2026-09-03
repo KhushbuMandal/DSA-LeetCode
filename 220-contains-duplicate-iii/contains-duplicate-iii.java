b1fc1418bb1a109503eec52cb23349ac509b3e90
KHUSHBU MANDAL <131984812+KhushbuMandal@users.noreply.github.com>
2026-09-03 13:37:07 +0530

    Time: 141 ms (35.91%) | Memory: 112.2 MB (61.34%) - LeetSync

---
+++ 220-contains-duplicate-iii/contains-duplicate-iii.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean containsNearbyAlmostDuplicate(
+        int[] nums,
+        int indexDiff,
+        int valueDiff
+    ) {
+
+        TreeSet<Long> set = new TreeSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            Long x = set.ceiling((long) nums[i] - valueDiff);
+
+            if (x != null && x <= (long) nums[i] + valueDiff) {
+                return true;
+            }
+
+            set.add((long) nums[i]);
+
+            if (i >= indexDiff) {
+                set.remove((long) nums[i - indexDiff]);
+            }
+        }
+
+        return false;
+    }
+}
